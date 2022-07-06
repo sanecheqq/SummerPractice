af588ccb21abf5a6bfab006dda24c8353bf2f88e
Lunidep <ilya151102@yandex.ru>
2022-07-06 19:31:59 +0300

    6th

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+
+from . import views
 from .views import index_page, player_page, landing_page
 
 urlpatterns = [
     path('', landing_page),
     path('base.html/', index_page),
+    path('search_movies/', views.search_movies, name='search_movies'),
     path('movie/<int:kinopoisk_id>', player_page),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import Movie
 
 def landing_page(request):
@@ -7,10 +9,24 @@ def landing_page(request):
 
 def index_page(request):
     movies = Movie.objects.all()
-    return render(request, 'movies/index.html', {'movies': movies})
+    return render(request, 'movies/movie_list.html', {'movies': movies})
 
 def player_page(request, kinopoisk_id):
     assert isinstance(kinopoisk_id, object)
     movie = Movie.objects.get(kinopoisk_id=kinopoisk_id)
     return render(request, 'movies/player.html', {'movie': movie})
 
+
+def search_movies(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        venues = Movie.objects.filter(name__contains=searched)
+
+        return render(request,
+                      'movies/search_movies.html',
+                      {'searched': searched,
+                       'venues': venues})
+    else:
+        return render(request,
+                      'movies/search_movies.html',
+                      {})
